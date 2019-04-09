46699ecb0785bbadb139d51b48daa48756748412
Ellis Saupe <ems236@case.edu>
2019-04-08 23:21:31 -0400

    Rear View toggle with c

---
+++ Assets/Scripts/UIControl.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIControl : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject rearViewCamera;
+
+    [SerializeField]
+    private GameObject rearViewCameraBorder;
+
+
+    private Image border;
+    private RawImage camera;
+
+    private bool enabled = true;
+    // Start is called before the first frame update
+    void Start()
+    {
+        border = rearViewCameraBorder.GetComponent<Image>();
+        camera = rearViewCamera.GetComponent<RawImage>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.C))
+        {
+            enabled = !enabled;
+            if(enabled)
+            {
+                border.enabled = true;
+                camera.enabled = true;
+            }
+            else
+            {
+                border.enabled = false;
+                camera.enabled = false;
+            }
+
+            //Debug.Log(enabled);
+        }
+    }
+}
