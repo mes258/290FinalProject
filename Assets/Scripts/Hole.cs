0f5ba1a645388b06751a60ab0fcc5e16e097415e
Ellis Saupe <ems236@case.edu>
2019-04-08 20:12:11 -0400

    Basic Holes

---
+++ Assets/Scripts/Hole.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Hole : MonoBehaviour
+{
+    [SerializeField]
+    public GameObject Ball;
+
+   //CapsuleCollider holeCollider;
+    // Start is called before the first frame update
+    void Start()
+    { 
+        //holeCollider = gameObject.GetComponent<CapsuleCollider>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject == Ball)
+        {
+            Debug.Log("BALL");
+        }
+        else
+        {
+            Debug.Log("PLAYER");
+        }
+    }
+}
