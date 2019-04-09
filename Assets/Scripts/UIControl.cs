465030a92f5039d10ddae6989f3e4cd2d39ca5b7
Ellis Saupe <ems236@case.edu>
2019-04-09 14:58:51 -0400

    Added basic menu controls

--- Assets/Scripts/UIControl.cs
+++ Assets/Scripts/UIControl.cs
@@ -11,16 +11,30 @@ public class UIControl : MonoBehaviour
     [SerializeField]
     private GameObject rearViewCameraBorder;
 
+    [SerializeField]
+    private GameObject levelEndGameObj;
+    private Canvas LevelEndCanvas;
+
+    [SerializeField]
+    private GameObject SettingsGameObj;
+    private Canvas SettingsCanvas;
 
     private Image border;
     private RawImage camera;
 
-    private bool enabled = true;
+    private bool cameraEnabled = true;
+    private bool settingsEnabled = false;
     // Start is called before the first frame update
     void Start()
     {
         border = rearViewCameraBorder.GetComponent<Image>();
         camera = rearViewCamera.GetComponent<RawImage>();
+
+        SettingsCanvas = SettingsGameObj.GetComponent<Canvas>();
+        SettingsCanvas.enabled = false;
+
+        LevelEndCanvas = levelEndGameObj.GetComponent<Canvas>();
+        LevelEndCanvas.enabled = false;
     }
 
     // Update is called once per frame
@@ -28,19 +42,51 @@ public class UIControl : MonoBehaviour
     {
         if (Input.GetKeyDown(KeyCode.C))
         {
-            enabled = !enabled;
-            if(enabled)
-            {
-                border.enabled = true;
-                camera.enabled = true;
-            }
-            else
-            {
-                border.enabled = false;
-                camera.enabled = false;
-            }
+            toggleRearCamera();
 
             //Debug.Log(enabled);
         }
+
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            toggleSettings();
+        }
+    }
+
+    void toggleRearCamera()
+    {
+        cameraEnabled = !cameraEnabled;
+        if (cameraEnabled)
+        {
+            border.enabled = true;
+            camera.enabled = true;
+        }
+        else
+        {
+            border.enabled = false;
+            camera.enabled = false;
+        }
+    }
+
+    void toggleSettings()
+    {
+        settingsEnabled = !settingsEnabled;
+        if(settingsEnabled)
+        {
+            Cursor.lockState = CursorLockMode.None;
+            Cursor.visible = true;
+            SettingsCanvas.enabled = true;
+        }
+        else
+        {
+            Cursor.lockState = CursorLockMode.Locked;
+            Cursor.visible = false;
+            SettingsCanvas.enabled = false;
+        }
+    }
+
+    void showLevelEnd()
+    {
+        LevelEndCanvas.enabled = true;
     }
 }
