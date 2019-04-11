78a2f250d895c77e37f05e8c70ae292bcc85e47b
Ellis Saupe <ems236@case.edu>
2019-04-11 17:13:22 -0400

    Made an enemy / static hazard

---
+++ Assets/Scripts/PlayerCollisionListener.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerCollisionListener : MonoBehaviour
+{
+    private const int ENEMYLAYER = 11;
+
+    private HUDController hud;
+    private PlayerSFXPlayer sfx;
+
+    private bool canhit = true;
+    // Start is called before the first frame update
+    void Start()
+    {
+        hud = GetComponent<HUDController>();
+        sfx = GetComponent<PlayerSFXPlayer>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnControllerColliderHit(ControllerColliderHit hit)
+    {
+        //Debug.Log(col.gameObject);
+        //if (hit.relativeVelocity.y > 3f)
+        //{
+        //    //sfx.playGroundHit();
+        //    //sfx.playJumpLand();
+        //}
+
+
+        if (canhit && hit.collider.gameObject.layer == ENEMYLAYER)
+        {
+            hud.addScore(1);
+            sfx.playDamage();
+            canhit = false;
+            StartCoroutine(delay());
+        }
+    }
+
+    private IEnumerator delay()
+    {
+        yield return new WaitForSeconds(1f);
+        canhit = true;
+    }
+}
