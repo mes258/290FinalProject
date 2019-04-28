12a6ecab8d4348e416fac7b5e4022820ad3fcf54
mes258 <michael.smith.singapore@gmail.com>
2019-04-28 11:47:55 -0400

    Updated score card

--- Assets/Scripts/EndCanvasController.cs
+++ Assets/Scripts/EndCanvasController.cs
@@ -6,27 +6,61 @@ using UnityEngine.SceneManagement;
 
 public class EndCanvasController : MonoBehaviour
 {
-    [SerializeField]
-    Text scoreDisplay;
+    int[] pars = { 3, 4, 5, 6, 5, 8, 14, 0, 60};
 
-    int[] pars = {3, 4, 5, 6, 5, 8, 14, 0, 60};
     // Start is called before the first frame update
     void Start()
     {
-        scoreDisplay.text += "Hole \tPar \t \tScore \r\n";
-        for (int i = 1; i <= 9; i++)
-        {
-            int score = HighScoreLogger.LookupScore(i);
-            string scoretext = score == -1 ? "None" : score.ToString();
-            //scoreDisplay.text += "Hole " + i + ": \t " + scoretext + "\r\n";
-            scoreDisplay.text += i + "\t\t\t" + pars[i-1] + "\t\t\t" + scoretext + "\r\n";
-        }
+
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    void OnGUI()
+    {
+        var h = 0.3f; // proportional height (0..1)
+        var windowX = (float)((Screen.width / 2) - 150f);
+        var windowY = (float)(Screen.height * (1 - (h + 0.2))) / 2;
+        var windowWidth = 300f;
+        var windowHeight = (float)(Screen.height * h);
+
+        Rect windowRect = new Rect(windowX, windowY, windowWidth, windowHeight);
+
+        windowRect = GUILayout.Window(0, windowRect, AddScores, "<b>Score Card</b>");
+    }
+
+    // Make the contents of the window
+    void AddScores(int windowID)
+    {
+        var centerAlignedLabel = new GUIStyle(GUI.skin.label);
+        centerAlignedLabel.alignment = TextAnchor.MiddleCenter;
+        //centerAlignedLabel.stretchWidth = true;
+        centerAlignedLabel.fixedWidth = 100;
+        centerAlignedLabel.fontSize = 20;
+
+        GUILayout.BeginVertical(GUILayout.Width(300));
+
+        GUILayout.BeginHorizontal();
+        GUILayout.Label("<b>Hole</b>", centerAlignedLabel);
+        GUILayout.Label("<b>Par</b>", centerAlignedLabel);
+        GUILayout.Label("<b>Score</b>", centerAlignedLabel);
+        GUILayout.EndHorizontal();
+        for (var i = 1; i < 10; i++)
+        {
+            int score = HighScoreLogger.LookupScore(i);
+            string scoretext = score == -1 ? "None" : score.ToString();
+
+            GUILayout.BeginHorizontal();
+            GUILayout.Label(i + "", centerAlignedLabel);
+            GUILayout.Label(pars[i - 1] + "", centerAlignedLabel);
+            GUILayout.Label(scoretext, centerAlignedLabel);
+            GUILayout.EndHorizontal();
+        }
+        GUILayout.EndVertical();
     }
 
     public void backToMenu()
