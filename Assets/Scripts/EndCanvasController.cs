ea0cf57f1bd4fa67405b820e071e2d9fc73ca564
mes258 <michael.smith.singapore@gmail.com>
2019-04-28 00:54:25 -0400

    Updated signs, end scene

--- Assets/Scripts/EndCanvasController.cs
+++ Assets/Scripts/EndCanvasController.cs
@@ -9,14 +9,17 @@ public class EndCanvasController : MonoBehaviour
     [SerializeField]
     Text scoreDisplay;
 
+    int[] pars = {3, 4, 5, 6, 5, 8, 14, 0, 60};
     // Start is called before the first frame update
     void Start()
     {
-        for(int i = 1; i <= 9; i++)
+        scoreDisplay.text += "Hole \tPar \t \tScore \r\n";
+        for (int i = 1; i <= 9; i++)
         {
             int score = HighScoreLogger.LookupScore(i);
             string scoretext = score == -1 ? "None" : score.ToString();
-            scoreDisplay.text += "Hole " + i + ": " + scoretext + "\r\n";
+            //scoreDisplay.text += "Hole " + i + ": \t " + scoretext + "\r\n";
+            scoreDisplay.text += i + "\t\t\t" + pars[i-1] + "\t\t\t" + scoretext + "\r\n";
         }
     }
 
